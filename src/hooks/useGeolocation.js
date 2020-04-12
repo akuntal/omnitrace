a175d3c09c532da031d48818d06b2fff0459e71b
Alok Kumar <kumaral@LP-C02XJ4P6JG5L.local>
2020-04-12 20:56:22 +0530

    changes

--- src/hooks/useGeolocation.js
+++ src/hooks/useGeolocation.js
@@ -4,10 +4,10 @@ import {Alert, PermissionsAndroid} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {updateGeolocation} from '../redux/actions';
 import {GEOLOCATION_DELAY} from '../utils';
+import Geolocation from 'react-native-geolocation-service';
 
-navigator.geolocation = require('@react-native-community/geolocation');
+Geolocation.setRNConfiguration({authorizationLevel: 'always'});
 
-navigator.geolocation.setRNConfiguration({authorizationLevel: 'always'});
 export const useGeolocation = () => {
   const geolocations = useSelector((state) => state.appState.geolocations);
   const isUserRegistered = useSelector(
@@ -15,42 +15,15 @@ export const useGeolocation = () => {
   );
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.CAMERA,
-          {
-            title: 'Location Permission',
-            message:
-              'Cool Photo App needs access to your camera ' +
-              'so you can take awesome pictures.',
-            buttonNeutral: 'Ask Me Later',
-            buttonNegative: 'Cancel',
-            buttonPositive: 'OK',
-          },
-        );
-        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-          console.log('You can use the camera');
-        } else {
-          console.log('Camera permission denied');
-        }
-      } catch (err) {
-        console.warn(err);
-      }
-    })();
-  }, []);
-
   const getGeolocation = () => {
-    if (navigator.geolocation) {
-      // navigator.geolocation.requestAuthorization();
-      navigator.geolocation.getCurrentPosition(
+    if (Geolocation) {
+      Geolocation.getCurrentPosition(
         ({timestamp, coords: {latitude, longitude}}) => {
+          console.log({timestamp, latitude, longitude});
           dispatch(updateGeolocation({timestamp, latitude, longitude}));
         },
         (error) => {
           console.log(error);
-          // Alert.alert('Error', JSON.stringify(error));
         },
         {
           enableHighAccuracy: true,
@@ -65,8 +38,22 @@ export const useGeolocation = () => {
   useEffect(() => {
     if (isUserRegistered) {
       getGeolocation();
-      BackgroundTimer.runBackgroundTimer(() => {
-        getGeolocation();
+      BackgroundTimer.runBackgroundTimer(async () => {
+        try {
+          const permission = await PermissionsAndroid.check(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          );
+          if (permission) {
+            getGeolocation();
+          } else {
+            Alert.alert(
+              'Location Permission not granted',
+              'This app needs location permission to work',
+            );
+          }
+        } catch (err) {
+          console.log(err);
+        }
       }, GEOLOCATION_DELAY);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
